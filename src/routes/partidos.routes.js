dc0cf35b06696ab07f0c8d8e43727a604665c7d1
Javier Uncos <javiersrfc30@gmail.com>
2026-02-06 14:28:37 -0300

    validacion de en las rutas post ,put

--- src/routes/partidos.routes.js
+++ src/routes/partidos.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { actualizarPartido, actualizarResultado, borrarPartido, crearPartido, obtenerPartidoPorId, obtenerPartidos } from "../controllers/partidos/partidos.controller.js";
 import { verifyAdmin, verifyTokenAuth } from "../middlewares/verifyToken.js";
+import { validatePartido, validatePartidoUpdate, validateResultado } from "../validators/partidosValidate.js";
+import { validate } from "../middlewares/validations.js";
 
 const router = Router();
 
-router.post("/partidos",verifyTokenAuth,verifyAdmin,crearPartido)
+router.post("/partidos",verifyTokenAuth,verifyAdmin,validatePartido,validate,crearPartido)
 router.get("/partidos",obtenerPartidos)
 router.get("/partidos/:id",obtenerPartidoPorId)
-router.put("/partidos/:id",verifyTokenAuth,verifyAdmin,actualizarPartido)
+router.put("/partidos/:id",verifyTokenAuth,verifyAdmin,validatePartidoUpdate,validate,actualizarPartido)
 router.delete("/partidos/:id",verifyTokenAuth,verifyAdmin,borrarPartido)
-router.put("/partidos/:id/resultado",verifyTokenAuth,verifyAdmin,actualizarResultado)
+router.put("/partidos/:id/resultado",verifyTokenAuth,verifyAdmin,validateResultado,validate,actualizarResultado)
 
 
 
